c9138e82cbef6b04d81abd092f7438ec817145e2
colton456p <colton.p@hotmail.com>
2024-11-16 15:27:38 -0800

    updated CICD tests

--- app/admin-backend/server.js
+++ app/admin-backend/server.js
@@ -2,17 +2,14 @@ require('dotenv').config();
 const express = require('express');
 
 const app = express();
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 6000;
 
-// Middleware
 app.use(express.json());
 
-// Routes
 app.get('/', (req, res) => {
   res.send('Welcome to the Admin Backend!');
 });
 
-// Start the server
 app.listen(PORT, () => {
   console.log(`Admin backend is running on port ${PORT}`);
 });
